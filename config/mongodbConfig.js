83be83b73feabee56c07f40300c8afc8f232d476
Nirajey-git <nirazgajurel21@gmail.com>
2024-09-28 13:52:30 +1000

    backend

--- config/mongodbConfig.js
+++ config/mongodbConfig.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-const MONGO_URL = "mongodb://localhost:27017/finance_tracker";
+const MONGO_URL = process.env.MONGO_URL;
 
-export const conMongoDb = async () => {
-  try {
-    const conn = await mongoose.connect(MONGO_URL);
-    conn && console.log("mongoDB connected");
-  } catch (error) {
-    console.log(error);
-  }
+export const conMongoDb = async() => {
+    try {
+        const conn = await mongoose.connect(MONGO_URL);
+        conn && console.log("mongoDB connected");
+    } catch (error) {
+        console.log(error);
+    }
 };
